527d7b6bd4fd36d03b53762a1e264f0404931378
h4ppyy <93immm@naver.com>
2021-01-13 02:40:40 +0900

    class to className

--- frontend/src/components/playground/Main.js
+++ frontend/src/components/playground/Main.js
@@ -40,31 +40,31 @@ const Main = () => {
   return (
     <>
       <div className="container">
-        <h1 class="text-center mt-5 mb-4 f1">상용한자 2136</h1>
+        <h1 className="text-center mt-5 mb-4 f1">상용한자 2136</h1>
 
         <div className="table-responsive-lg">
           <table className="table table-borderless f2">
             <thead>
               <tr>
-                <th class="text-center" scope="col">번호</th>
-                <th class="text-center" scope="col">뜻</th>
-                <th class="text-center" scope="col">한자</th>
-                <th class="text-center" scope="col">훈독</th>
-                <th class="text-center" scope="col">음독</th>
-                <th class="text-center" scope="col">단어확인</th>
+                <th className="text-center" scope="col">번호</th>
+                <th className="text-center" scope="col">뜻</th>
+                <th className="text-center" scope="col">한자</th>
+                <th className="text-center" scope="col">훈독</th>
+                <th className="text-center" scope="col">음독</th>
+                <th className="text-center" scope="col">단어확인</th>
               </tr>
             </thead>
             <tbody>
               {
-                loop.map(() => {
+                loop.map((item, idx) => {
                   return (
-                    <tr>
-                      <th class="text-center" scope="row">1</th>
+                    <tr key={idx}>
+                      <th className="text-center" scope="row">1</th>
                       <td>깨달을 각</td>
-                      <td class="text-center">覚</td>
+                      <td className="text-center">覚</td>
                       <td>かく</td>
                       <td>おぼえる、さます、さめる</td>
-                      <td class="text-center">
+                      <td className="text-center">
                         <button
                           className="btn btn-outline-light"
                           type="button"
