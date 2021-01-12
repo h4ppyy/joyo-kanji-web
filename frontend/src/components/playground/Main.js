3a6244e20eb0a198a987e2d5de3f9cff8866cbdb
h4ppyy <93immm@naver.com>
2021-01-13 02:19:45 +0900

    dark mode

--- frontend/src/components/playground/Main.js
+++ frontend/src/components/playground/Main.js
@@ -37,48 +37,50 @@ const Main = () => {
   ]
 
   return (
-    <div className="container">
-      <h1 class="text-center mt-5 mb-4">상용한자 2136</h1>
+    <>
+      <div className="container">
+        <h1 class="text-center mt-5 mb-4 f1">상용한자 2136</h1>
 
-      <div className="table-responsive-lg">
-        <table className="table table-bordered">
-          <thead>
-            <tr>
-              <th scope="col">번호</th>
-              <th scope="col">뜻</th>
-              <th scope="col">한자</th>
-              <th scope="col">훈독</th>
-              <th scope="col">음독</th>
-              <th scope="col">단어확인</th>
-            </tr>
-          </thead>
-          <tbody>
-            {
-              loop.map(() => {
-                return (
-                  <tr>
-                    <th scope="row">1</th>
-                    <td>깨달을 각</td>
-                    <td>覚</td>
-                    <td>かく</td>
-                    <td>おぼえる、さます、さめる</td>
-                    <td>
-                      <button
-                        className="btn btn-primary"
-                        type="button"
-                        onClick={() => showWord()}
-                      >
-                        단어
-                  </button>
-                    </td>
-                  </tr>
-                )
-              })
-            }
-          </tbody>
-        </table>
+        <div className="table-responsive-lg">
+          <table className="table table-borderless f2">
+            <thead>
+              <tr>
+                <th class="text-center" scope="col">번호</th>
+                <th class="text-center" scope="col">뜻</th>
+                <th class="text-center" scope="col">한자</th>
+                <th class="text-center" scope="col">훈독</th>
+                <th class="text-center" scope="col">음독</th>
+                <th class="text-center" scope="col">단어확인</th>
+              </tr>
+            </thead>
+            <tbody>
+              {
+                loop.map(() => {
+                  return (
+                    <tr>
+                      <th class="text-center" scope="row">1</th>
+                      <td>깨달을 각</td>
+                      <td class="text-center">覚</td>
+                      <td>かく</td>
+                      <td>おぼえる、さます、さめる</td>
+                      <td class="text-center">
+                        <button
+                          className="btn btn-outline-light"
+                          type="button"
+                          onClick={() => showWord()}
+                        >
+              단어
+                    </button>
+                      </td>
+                    </tr>
+                  )
+                })
+              }
+            </tbody>
+          </table>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
