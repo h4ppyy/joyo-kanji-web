7bb48a307ea1572d1f59e25f5a881dc3476eea74
h4ppyy <93immm@naver.com>
2021-01-14 04:18:48 +0900

    write kanji class, word class in admin.py

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
 
-from . import models
+from .models import Kanji, Word
 
-admin.site.register(models.Kanji)
-admin.site.register(models.Word)
+@admin.register(Kanji)
+class KanjiAdmin(admin.ModelAdmin):
+    list_display = ['id', 'kanji', 'mean', 'kunyomi', 'onyomi']
+    ordering = ['id']
+    search_fields = ['id', 'kanji', 'mean', 'kunyomi', 'onyomi']
+
+@admin.register(Word)
+class WordAdmin(admin.ModelAdmin):
+    list_display = ['id', 'kanji_kanji', 'image', 'word', 'mean']
+    ordering = ['-id']
+    search_fields = ['word', 'mean']
+    
+    def kanji_kanji(self, obj):
+        return obj.kanji.kanji
