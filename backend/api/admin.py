3441068f4d1d2941d1429a1f2ce66af1662ef008
h4ppyy <93immm@naver.com>
2021-01-14 17:02:59 +0900

    change foreign key display in django admin form

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-
 from .models import Kanji, Word
 
 @admin.register(Kanji)
@@ -13,6 +12,25 @@ class WordAdmin(admin.ModelAdmin):
     list_display = ['id', 'kanji_kanji', 'image', 'word', 'mean']
     ordering = ['-id']
     search_fields = ['word', 'mean']
-    
+
     def kanji_kanji(self, obj):
         return obj.kanji.kanji
+
+'''
+class Invoice(models.Model):
+      person = models.ForeignKey(Person)
+      ....
+
+class InvoiceAdmin(admin.ModelAdmin):
+      form = MyInvoiceAdminForm
+
+
+class MyInvoiceAdminForm(forms.ModelForm):
+    person = CustomModelChoiceField(queryset=Person.objects.all()) 
+    class Meta:
+          model = Invoice
+      
+class CustomModelChoiceField(forms.ModelChoiceField):
+     def label_from_instance(self, obj):
+         return "%s %s" % (obj.first_name, obj.last_name)
+'''
--- backend/api/models.py
+++ backend/api/models.py
@@ -8,6 +8,9 @@ class Kanji(models.Model):
     kunyomi = models.CharField(max_length=255, blank=True, null=True)
     onyomi = models.CharField(max_length=255, blank=True, null=True)
 
+    def __str__(self):
+        return '{kanji} - {mean}'.format(kanji=self.kanji, mean=self.mean)
+
 class Word(models.Model):
     kanji = models.ForeignKey(Kanji, on_delete=models.CASCADE)
     image = models.ImageField(upload_to=os.path.join(settings.UPLOAD_DIR, 'word'))
